17632b71abbbc88c733906fb5ab74adec640a9f3
Adam Anderson <adama@fnal.gov>
2018-09-30 19:06:06 +0000

    Add ability to change pinout with argument to __init__

--- TB6612FNG.py
+++ TB6612FNG.py
@@ -1,27 +1,33 @@
 from RPi import GPIO
 
-pinout = {'PWMA': 18,
-          'AI1':  27,
-          'AI2':  22,
-          'STBY': 17}
 
 class TB6612FNG(object):
-    def __init__(self):
+    def __init__(self, pinout=None):
+        if pinout is None:
+            self.pinout = {'PWMA': 18,
+                           'AI1':  27,
+                           'AI2':  22,
+                           'STBY': 17}
+        else:
+            if set(pinout.keys()) != {'STBY', 'AI2', 'PWMA', 'AI1'}:
+                raise ValueError('Pinout does not contain correct pin names.')
+            self.pinout = pinout
+        
         GPIO.setmode(GPIO.BCM)
-        for pinname in pinout:
-            GPIO.setup(pinout[pinname], GPIO.OUT, initial=0)
-        self.pwm = GPIO.PWM(pinout['PWMA'], 200)
+        for pinname in self.pinout:
+            GPIO.setup(self.pinout[pinname], GPIO.OUT, initial=0)
+        self.pwm = GPIO.PWM(self.pinout['PWMA'], 200)
         self.direction = 'CW'
         
     def power_on(self, dutycycle):
         # check that the output pins match the software direction
         self.set_direction(self.direction)
-        GPIO.output(pinout['STBY'], GPIO.HIGH)
+        GPIO.output(self.pinout['STBY'], GPIO.HIGH)
         self.pwm.start(dutycycle)
 
     def power_off(self):
-        GPIO.output(pinout['AI1'], GPIO.LOW)
-        GPIO.output(pinout['AI2'], GPIO.LOW)
+        GPIO.output(self.pinout['AI1'], GPIO.LOW)
+        GPIO.output(self.pinout['AI2'], GPIO.LOW)
         self.pwm.stop()
         
     def set_pwm(self, dutycycle):
@@ -29,11 +35,11 @@ class TB6612FNG(object):
 
     def set_direction(self, direction):
         if direction is 'CW':
-            GPIO.output(pinout['AI1'], GPIO.HIGH)
-            GPIO.output(pinout['AI2'], GPIO.LOW)
+            GPIO.output(self.pinout['AI1'], GPIO.HIGH)
+            GPIO.output(self.pinout['AI2'], GPIO.LOW)
         elif direction is 'CCW':
-            GPIO.output(pinout['AI1'], GPIO.LOW)
-            GPIO.output(pinout['AI2'], GPIO.HIGH)
+            GPIO.output(self.pinout['AI1'], GPIO.LOW)
+            GPIO.output(self.pinout['AI2'], GPIO.HIGH)
         else:
             raise ValueError('Argument `direction` must be string "CW" or "CCW".')
 
